443f62da614439343aab657f06a0bfb9c4659fe8
yyeonkim <seyeon.dev@gmail.com>
2021-11-16 14:58:56 +0900

    Feat: Add icons to images.js

---
+++ src/images.js
@@ -0,0 +1,9 @@
+import CheckedBox from "../assets/baseline_check_box_outline_blank_black_24dp.png";
+import Add from "../assets/outline_add_black_24dp.png";
+import UncheckedBox from "../assets/baseline_check_box_outline_blank_black_24dp.png";
+
+export const images = {
+  uncomplete: UncheckedBox,
+  complete: CheckedBox,
+  add: Add,
+};
