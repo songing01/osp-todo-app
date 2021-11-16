b4a67550de2e7b8cb8e7d62f69dbb46d6ca831f8
yyeonkim <seyeon.dev@gmail.com>
2021-11-16 21:58:09 +0900

    Feat: Add move icon to images.js

--- src/images.js
+++ src/images.js
@@ -1,9 +1,11 @@
 import CheckedBox from "../assets/baseline_check_box_outline_blank_black_24dp.png";
 import Add from "../assets/outline_add_black_24dp.png";
 import UncheckedBox from "../assets/baseline_check_box_outline_blank_black_24dp.png";
+import Move from "../assets/outline_import_export_black_24dp.png";
 
 export const images = {
   uncomplete: UncheckedBox,
   complete: CheckedBox,
   add: Add,
+  move: Move,
 };
